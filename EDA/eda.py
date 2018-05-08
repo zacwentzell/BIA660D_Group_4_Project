1c693d127efe70034742f7ddc193510494a5d3be
haocheng <hcheng7@stevens.edu>
2018-05-07 23:32:35 -0400

    eda

--- EDA/eda.py
+++ EDA/eda.py
@@ -1,34 +1,66 @@
 # coding: utf-8
 
-# In[32]:
+# In[1]:
+
 
 import pandas as pd
 import numpy as np
 import re
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 # In[2]:
+
 
 pop_lyrics = pd.read_csv('lyrics_pop_300.csv')
 del (pop_lyrics['Unnamed: 0'])
 pop_lyrics
 
-# In[3]:
+# In[13]:
 
-country_lyrics = pd.read_csv('lyrics_country_299.csv')
+
+p = pop_lyrics.Artist.value_counts()
+p
+
+# In[14]:
+
+
+plt.hist(p)
+plt.show()
+
+# In[16]:
+
+
+country_lyrics = pd.read_csv(
+    'lyrics_country_299.csv')
 del (country_lyrics['Unnamed: 0'])
 country_lyrics
 
-# In[4]:
+# In[17]:
+
+
+c = country_lyrics.Artist.value_counts()
+c
+
+# In[19]:
+
+
+plt.hist(c)
+plt.show()
+
+# In[6]:
+
 
 general_lyrics = pop_lyrics.append(country_lyrics)
 
-# In[5]:
+# In[7]:
+
 
 general_lyrics
 
 
-# In[6]:
+# In[8]:
+
 
 def StopStem(df):
     stop = []
@@ -43,50 +75,51 @@ def StopStem(df):
     return df
 
 
-# In[7]:
+# In[9]:
+
 
 general_lyrics = StopStem(general_lyrics)
 
-# In[8]:
+# In[10]:
+
 
 general_lyrics
 
-# In[9]:
+# In[11]:
+
 
 import matplotlib.pyplot as plt
 
-# In[10]:
+# In[12]:
+
 
 general_lyrics['Lyrics_length'] = general_lyrics.Lyrics.apply(lambda x: len(x))
 
 # In[13]:
 
+
 general_lyrics.Lyrics_length.describe()
 
-# In[15]:
+# In[32]:
+
 
 general_lyrics.Lyrics_length.plot(kind='kde', logx=True)
 plt.show()
 
-# In[16]:
+# In[15]:
+
 
 general_lyrics['Genre_type'] = general_lyrics.Genre.apply(lambda x: 0 if 'Pop' in x else 1)
 
-# In[19]:
+# In[33]:
+
 
 general_lyrics.plot(kind="scatter", x="Genre_type", y="Lyrics_length", alpha=0.2)
 plt.show()
 
-# In[20]:
 
-pop_lyrics.Artist.value_counts()
+# In[17]:
 
-# In[21]:
-
-country_lyrics.Artist.value_counts()
-
-
-# In[33]:
 
 def cleanText(corpus):
     preprocessed_sentences = []
@@ -95,8 +128,6 @@ def cleanText(corpus):
     pos_to_keep = ('NOUN', 'PROPN', 'ADJ', 'VERB')
     for doc in nlp.pipe(corpus, n_threads=8):
         keep_tokens_string = ' '.join([t.lemma_ for t in doc if t.pos_ in pos_to_keep and t.lemma_ not in stop_words])
-        #         doc = nlp(lyrics)
-        #         lyrics = ' '.join([t.lemma_ for t in doc if t.pos_ in pos_to_keep and t.lemma_ not in stop_words])
         preprocessed_sentences.append(keep_tokens_string)
     preprocessed_sentences = [z.lower().replace('\n', ' ').split() for z in preprocessed_sentences]
     for sentence in preprocessed_sentences:
@@ -105,7 +136,8 @@ def cleanText(corpus):
     return word_bag
 
 
-# In[34]:
+# In[18]:
+
 
 from sklearn.feature_extraction import stop_words
 
@@ -126,42 +158,139 @@ def StopStem(df):
     return df
 
 
-# In[35]:
+# In[48]:
+
 
 pop_lyrics = StopStem(pop_lyrics)
 
-# In[36]:
+# In[20]:
+
 
 country_lyrics = StopStem(country_lyrics)
 
-# In[37]:
+# In[21]:
+
 
 import spacy
 
 nlp = spacy.load('en')
 
-# In[38]:
+# In[22]:
+
 
 X = pop_lyrics.loc[:, 'Lyrics']
 
-# In[39]:
+# In[23]:
+
 
 X
 
-# In[40]:
+# In[24]:
+
 
 pop_word_bag = cleanText(X)
 
-# In[41]:
+# In[25]:
+
 
 pop_word_bag
 
-# In[47]:
+# In[26]:
 
-wordcloud_pop = WordCloud(background_color="white", width=5000, height=3000, max_words=200).generate(
-    ' '.join(pop_word_bag))
+
+unique_words_pop = list(set(pop_word_bag))
+
+freq_pop = []
+for word in unique_words_pop:
+    freq_pop.append((word, pop_word_bag.count(word)))
+
+# sort
+freq_pop.sort(key=lambda x: x[1], reverse=True)
+
+# In[27]:
+
+
+freq_pop
+
+# In[28]:
+
+
+pop_word_bag = []
+for i in freq_pop:
+    for w in range(i[1]):
+        pop_word_bag.append(i[0])
+
+pop_word_bag
+
+# In[29]:
+
+
+# wc = WordCloud(background_color="white",width=5000, height=3000, max_words=200,random_state=42)
+
 
 # In[50]:
+
+
+Y = country_lyrics.loc[:, 'Lyrics']
+Y
+
+# In[51]:
+
+
+country_word_bag = cleanText(Y)
+country_word_bag
+
+# In[52]:
+
+
+unique_words_country = list(set(country_word_bag))
+
+freq_country = []
+for word in unique_words_country:
+    freq_country.append((word, country_word_bag.count(word)))
+
+# sort
+freq_country.sort(key=lambda x: x[1], reverse=True)
+
+# In[53]:
+
+
+freq_country
+
+# In[54]:
+
+
+country_word_bag = []
+for i in freq_country:
+    for w in range(i[1]):
+        country_word_bag.append(i[0])
+
+country_word_bag
+
+# In[55]:
+
+
+wordcloud_country = WordCloud(background_color="white", width=1000, height=1000, collocations=False, ).generate(
+    ' '.join(country_word_bag))
+
+# In[56]:
+
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(20, 15))
+plt.imshow(wordcloud_country)
+plt.axis("off")
+plt.show()
+
+# In[57]:
+
+
+wordcloud_pop = WordCloud(background_color="white", width=1000, height=1000, collocations=False, ).generate(
+    ' '.join(pop_word_bag))
+
+# In[58]:
+
 
 import matplotlib.pyplot as plt
 
@@ -170,31 +299,100 @@ plt.imshow(wordcloud_pop)
 plt.axis("off")
 plt.show()
 
-# In[51]:
+# In[59]:
 
-X = country_lyrics.loc[:, 'Lyrics']
 
-# In[54]:
+common_word = []
 
-country_word_bag = cleanText(X)
+for i in freq_pop:
+    for j in freq_country:
+        if i[0] == j[0]:
+            common_word.append(i[0])
+common_word
 
-# In[55]:
+# In[60]:
 
-country_word_bag
 
-# In[56]:
+pop_word_bag_unique = []
+for i in pop_word_bag:
+    if i not in common_word:
+        pop_word_bag_unique.append(i)
+pop_word_bag_unique
 
-wordcloud_country = WordCloud(background_color="white", width=5000, height=3000, max_words=200).generate(
-    ' '.join(country_word_bag))
+# In[61]:
 
-# In[57]:
+
+country_word_bag_unique = []
+for i in country_word_bag:
+    if i not in common_word and i.isalpha():
+        country_word_bag_unique.append(i)
+country_word_bag_unique
+
+# In[62]:
+
+
+wordcloud_country = WordCloud(background_color="white", width=1000, height=1000, collocations=False).generate(
+    ' '.join(country_word_bag_unique))
+
+# In[63]:
+
 
 plt.figure(figsize=(20, 15))
 plt.imshow(wordcloud_country)
 plt.axis("off")
 plt.show()
 
-# In[ ]:
+# In[64]:
 
 
+wordcloud_pop = WordCloud(background_color="white", width=1000, height=1000, collocations=False, ).generate(
+    ' '.join(pop_word_bag_unique))
+
+# In[65]:
+
+
+plt.figure(figsize=(20, 15))
+plt.imshow(wordcloud_pop)
+plt.axis("off")
+plt.show()
+
+# In[68]:
+
+
+X1 = pop_lyrics.loc[:, 'Artist']
+X1.values
+
+# In[69]:
+
+
+wordcloud_pop_artists = WordCloud(background_color="white", width=1000, height=1000, collocations=False, ).generate(
+    ' '.join(X1))
+
+# In[70]:
+
+
+plt.figure(figsize=(20, 15))
+plt.imshow(wordcloud_pop_artists)
+plt.axis("off")
+plt.show()
+
+# In[71]:
+
+
+Y1 = country_lyrics.loc[:, 'Artist']
+Y1.values
+
+# In[73]:
+
+
+wordcloud_country_artists = WordCloud(background_color="white", width=1000, height=1000, collocations=False, ).generate(
+    ' '.join(Y1))
+
+# In[74]:
+
+
+plt.figure(figsize=(20, 15))
+plt.imshow(wordcloud_country_artists)
+plt.axis("off")
+plt.show()
 
